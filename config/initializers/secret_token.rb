cabcec05a26becb82b148e0cafe21b0a62f60752
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-01-29 08:53:14 +0000

    initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demaths::Application.config.secret_token = '47d9582fa14887ee68baf33c0ca227f2714b859b178e4198c1b6c9f38bb6ecece591e2a6a8a49c10b5513f0b2027cd128a60d7b422275124b42ad70b38258db5'
