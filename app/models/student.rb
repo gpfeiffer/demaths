5236f2be37c7c92c61b39436d9fa27441d8db97d
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-01-31 14:41:07 +0000

    polished student views

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,9 @@
 class Student < ActiveRecord::Base
   attr_accessible :affiliation, :first_name, :last_name
+
+  default_scope :order => [:last_name, :first_name]
+
+  def first_last
+    "#{first_name} #{last_name}"
+  end
 end
