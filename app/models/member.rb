c6e7f22d4e25c3ebb8260f305eba2445c95a3ad5
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-01-31 14:12:53 +0000

    name

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,8 @@
 class Member < ActiveRecord::Base
   attr_accessible :affiliation, :first_name, :last_name
 
+  default_scope :order => [:last_name, :first_name]
+
   def first_last
     "#{first_name} #{last_name}"
   end
