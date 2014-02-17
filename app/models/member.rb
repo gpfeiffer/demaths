c01a3a37657d8c365ec2b74171c63653942d3e89
Goetz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-02-17 14:59:02 +0000

    copied iris content

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Member < ActiveRecord::Base
   attr_accessible :affiliation, :first_name, :iris_url, :last_name
 
@@ -24,4 +26,22 @@ class Member < ActiveRecord::Base
   def full_iris_url
     "http://www.nuigalway.ie/our-research/people/mathematics-statistics-and-applied-mathematics/#{iris_url}/"
   end
+
+  def iris_home
+    page = Nokogiri::HTML(open(full_iris_url))
+    page.css("div[title='Home']").to_html
+  end
+
+  def iris_publications
+    page = Nokogiri::HTML(open(full_iris_url))
+    page.css("div[title='Publications']").to_html
+  end
+
+  def iris_content
+    content = Nokogiri::HTML(open(full_iris_url)).css("div[title]")
+    image = content.css("img[alt='My Photograph']").first
+    image["src"] = URI.join(full_iris_url, image["src"]).to_s
+    content
+  end
+
 end
