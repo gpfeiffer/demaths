48e66cb6178dfece923e457c604364ef9966f6dc
Goetz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-03-07 10:41:31 +0000

    mv'd calendar to side, display top 5 posts on front

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def index
+    @posts = Post.top(5)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   attr_accessible :author_id, :body, :published_at, :title
 
   belongs_to :author, :class_name => 'Role'
+
+  # find the top n posts according to some order
+  def self.top n
+    Post.all.sort_by(&:published_at).last n
+  end
 end
