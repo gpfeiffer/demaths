7153c29e62b1cb668870227bd08ef388f1449636
Goetz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-03-10 11:49:40 +0000

    post permissions.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,22 +10,22 @@ class Ability
       can [:update], User do |u|
         u == user
       end
-      can [:read], [Member, Student]
+      can [:read], [Member, Student, Post]
+    end
+    if user.member?
       can [:update, :destroy], Member do |member|
         member.user == user
       end
-      can [:update, :destroy], Student do |student|
-        student.user == user
-      end
-    end
-    if user.member?
-      can [:create, :read], Post
+      can [:create], Post
       can [:update, :destroy], Post do |post|
         post.author == user.role
       end
     end
     if user.student?
-      can [:create, :read], Post
+      can [:update, :destroy], Student do |student|
+        student.user == user
+      end
+      can [:create], Post
       can [:update, :destroy], Post do |post|
         post.author == user.role
       end
